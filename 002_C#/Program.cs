ef0a0fe30d1af67cee301910d682a62ef4e45656
smartonc3 <smartonc3@inbox.lv>
2023-03-16 16:23:14 +0000

    002 сложения

--- 002_C#/Program.cs
+++ 002_C#/Program.cs
@@ -1,5 +1,9 @@
-﻿Console.WriteLine("Введите Има");
-string username = Console.ReadLine();
-Console.WriteLine("Привет, ");
-Console.WriteLine(username);
+﻿// Console.WriteLine("Введите Има");
+// string username = Console.ReadLine();
+// Console.WriteLine("Привет, ");
+// Console.WriteLine(username);
 
+int numberA = 3;
+int namberb = 5;
+int result = numberA + namberb;
+Console.WriteLine(result);
