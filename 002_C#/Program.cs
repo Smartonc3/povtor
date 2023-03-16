bbdc85881bc750274ed03465ea4559f55416b6f4
smartonc3 <smartonc3@inbox.lv>
2023-03-16 16:53:20 +0000

    002 гири

--- 002_C#/Program.cs
+++ 002_C#/Program.cs
@@ -19,14 +19,33 @@
 
 
 
-Console.Write("Введите имя:  ");
-string username = Console.ReadLine();
+// Console.Write("Введите имя:  ");
+// string username = Console.ReadLine();
 
-if(username.ToLower() == "маша")
-{
-    Console.WriteLine("Ура маша");
-}
-else
-{
-    Console.WriteLine("Привет -  " + username);
-}
+// if(username.ToLower() == "маша")
+// {
+//     Console.WriteLine("Ура маша");
+// }
+// else
+// {
+//     Console.WriteLine("Привет -  " + username);
+// }
+
+
+int a = 1;
+int b = 2;
+int c = 6;
+int d = 8;
+int e = 4;
+
+int max = a;
+
+if (a > max ) max = a;
+if (b > max ) max = b;
+if (c > max ) max = c;
+if (d > max ) max = d;
+if (e > max ) max = e;
+
+
+Console.Write("max = ");
+Console.WriteLine(max);
