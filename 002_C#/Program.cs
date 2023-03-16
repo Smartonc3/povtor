1acc327722e2b3174cc71d3e87fb4556125e8431
smartonc3 <smartonc3@inbox.lv>
2023-03-16 16:27:57 +0000

    002 деления

--- 002_C#/Program.cs
+++ 002_C#/Program.cs
@@ -3,7 +3,13 @@
 // Console.WriteLine("Привет, ");
 // Console.WriteLine(username);
 
-int numberA = 3;
-int namberb = 5;
-int result = numberA + namberb;
-Console.WriteLine(result);
+// int numberA = 3;
+// int namberb = 5;
+// int result = numberA + namberb;
+// Console.WriteLine(result);
+
+
+
+double numberA = 12;
+double namberb = 5;
+Console.WriteLine(numberA / namberb);
